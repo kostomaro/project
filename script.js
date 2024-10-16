372f278916cb3b61c717aa0f8c71920f3b99ffd4
Nazar <kostomaro23234@gmail.com>
2024-10-16 19:05:02 +0300

    first coomit

---
+++ script.js
@@ -0,0 +1,14 @@
+let und;
+console.log(und);
+
+const obj = {
+    name: "Назар",
+    age: 25,
+    isMarried: true
+};
+
+console.log(obj["name"]);
+
+let arr = [1, 2, 3, 4, 5, 6, "plum.png", {name: "Назар"}, 2,]
+
+console.log(arr[3])
